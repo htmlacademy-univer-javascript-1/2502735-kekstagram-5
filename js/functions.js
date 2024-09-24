9b46a054aa16c7fd7e911ee5e162bd3c34a7b712
Ksenia1102 <shiro.keyyu13@bk.ru>
2024-09-24 10:47:17 +0500

    Проверка работы функций

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+// Функция для проверки длины строки
+function checkStringLenght(str, maxLength) {
+    return str.length <= maxLength;
+  }
+
+  // Примеры:
+  console.log(checkStringLenght('проверяемая строка', 20)); // true
+  console.log(checkStringLenght('проверяемая строка', 18)); // true
+  console.log(checkStringLenght('проверяемая строка', 10)); // false
+
+// Функция для проверки, является ли строка палиндромом
+  function checkPalindrome(str) {
+    const processedStr = str.toLowerCase().replaceAll(' ', '');
+
+    const reversedStr = processedStr.split('').reverse().join('');
+
+    return processedStr == reversedStr;
+  }
+
+  // Примеры:
+  console.log(checkPalindrome('топот')); // true
+  console.log(checkPalindrome('ДовОд')); // true
+  console.log(checkPalindrome('Кекс'));  // false
+  console.log(checkPalindrome('Лёша на полке клопа нашёл ')); // true
